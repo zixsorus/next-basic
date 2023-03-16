9d526570d42e4011f930498e2e00612ae6a3af5c
zixsorus <touy_elec@hotmail.com>
2023-03-16 09:22:59 +0700

    optimize font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.css'
 import Layout from './components/Layout'
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({
+  weight: ['400','700'],
+  subsets: ['thai']
+})
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={kanit.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   )
 }
